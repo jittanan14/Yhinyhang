029b44443dab3a8118c57664a1edb1c98f75e26a
jittanan <jittanan@MacBook-Pro-khxng-jittanan.local>
2019-05-22 16:44:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jittanan/yhinyhang/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.jittanan.yhinyhang;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+    }
+}
