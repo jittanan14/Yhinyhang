10830c6a16f948f019a3a542bc8d8070440526b4
jittanan <srisawat_j3@silpakorn.edu>
2019-07-30 16:11:49 +0700

    fix question page

--- app/src/main/java/com/example/jittanan/yhinyhang/Question.java
+++ app/src/main/java/com/example/jittanan/yhinyhang/Question.java
@@ -1,5 +1,4 @@
 package com.example.jittanan.yhinyhang;
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,12 +7,12 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class Question extends AppCompatActivity {
 
     ArrayList<String> question;
+    ArrayList<Integer> answer;
     TextView Text_question ;
     Button button_next ;
     Button button_previous;
@@ -22,8 +21,8 @@ public class Question extends AppCompatActivity {
     RadioButton radioButton3;
     RadioButton radioButton4;
     RadioButton radioButton5;
-
     RadioButton[] radioAnswer;
+
 
     int index = 1;
 
@@ -32,6 +31,8 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         getSupportActionBar().hide();
+
+
 
         Text_question = findViewById(R.id.Text_question);
         button_next = findViewById(R.id.button_next_question);
@@ -42,128 +43,142 @@ public class Question extends AppCompatActivity {
         radioButton4 = findViewById(R.id.radioButton4);
         radioButton5 = findViewById(R.id.radioButton5);
 
-        radioAnswer = new RadioButton[5];
-        radioAnswer[0] = radioButton1;
-        radioAnswer[1] = radioButton2;
-        radioAnswer[2] = radioButton3;
-        radioAnswer[3] = radioButton4;
-        radioAnswer[4] = radioButton5;
+        radioAnswer = new RadioButton[6];
+        radioAnswer[0] = null;
+        radioAnswer[1] = radioButton1;
+        radioAnswer[2] = radioButton2;
+        radioAnswer[3] = radioButton3;
+        radioAnswer[4] = radioButton4;
+        radioAnswer[5] = radioButton5;
 
         setQuestion();
 
         button_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                radioButton1.setSelected(false);
-                Log.e("Question", String.valueOf(radioButton1.isChecked()));
-
-                radioButton1.setChecked(false);
-
-                updateAnswer();
-
-                String point_question = Checkpoint();
-                if (!point_question.isEmpty()) {
+                if(checkPoint() != 0) {
                     updateQuestion();
-                }
-                else {
+                    updateAnswer();
+                } else {
                     Toast.makeText(Question.this, "กรุณาเลือกระดับคะแนน", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
-//
-//        button_previous.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String point_question = Checkpoint();
-//                if (!point_question.isEmpty()) {
-//                    for (int i = 0; i < question.size(); i++) {
-//                        Text_question.setText(i + 1 + " " + question.get(i));
-//                    }
-//
-//                }else {
-//                    Toast.makeText(Question.this, "กรุณาเลือกระดับคะแนน", Toast.LENGTH_SHORT).show();
-//
-//                }
-//            }
-//        });
+
+        button_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkPoint() != 0) {
+                   backward_question();
+                } else {
+                    Toast.makeText(Question.this, "กรุณาเลือกระดับคะแนน", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
     public void updateQuestion() {
+        index++;
         Text_question.setText(index+". "+question.get(index));
 
-        index++;
+        if (index != 1){
+            button_previous.setVisibility(View.VISIBLE);
+        }
+        if (index == 21) {
+            button_next.setVisibility(View.GONE);
+        }
+
+        answer.add(checkPoint());
+
+        Log.e("QQ", Integer.toString(checkPoint()));
+
+        Log.e("QQ2", String.valueOf(radioButton1.isChecked()));
+
+
+
+
+
+
     }
 
     public void updateAnswer() {
-        for(int i=0; i<radioAnswer.length; i++) {
+        for(int i=1; i<radioAnswer.length; i++) {
             radioAnswer[i].setChecked(false);
         }
     }
 
-    public int Random_question(){
-
-        int j = 0;
-
-        for (int i=0; i< question.size(); i++) {
-            j=i;
-            return j ;
+    public void backward_question(){
+        index--;
+        Text_question.setText(index+". "+question.get(index));
+        if (index == 1 ){
+            button_previous.setVisibility(View.GONE);
+        }else {
+            button_next.setVisibility(View.VISIBLE);
         }
 
-
-    return 0;
+        radioAnswer[answer.get(index)].setChecked(true);
 
     }
 
+
     public void setQuestion(){
         question = new ArrayList<>();
+        answer = new ArrayList<>();
 
         question.add(" ");
+        answer.add(0);
+
         //yhin
         question.add("คุณมีอาการหน้าซีดระดับใด ?");
         question.add("คุณหายใจเบาระดับใด ?");
         question.add("คุณขี้หนาวระดับใด ?");
         question.add("คุณรู้สึกไม่ค่อยมีแรงระดับใด ?");
         question.add("คุณอุจจาระน้อยและค่อนข้างเหลวระดับใด?");
-        question.add("คุณปัสสาวะมากและใสระดับใด ?");
+        question.add("คุณปัสสาวะมาก ระดับใด ?");
         question.add("คุณรู้สึกท้องอืดระดับใด ?");
-        question.add("คุณร้อนในระดับใด ?");
+        question.add("คุณร้อนใน ระดับใด ?");
         question.add("คุณปากแห้งระดับใด ?");
-        question.add("คุณคอแห้งหรือไม่ ?");
-        question.add("คุณข้ีหงุดหงิดหรือไม่ ?");
-        question.add("คุณผิวแห้งหรือไม่ ?");
-        question.add("คุณฝ่ามือและฝ่าเท้าร้อนหรือไม่ ?");
+        question.add("คุณคอแห้งระดับใด ?");
+        question.add("คุณข้ีหงุดหงิดระดับใด ?");
+        question.add("คุณผิวแห้งระดับใด ?");
+        question.add("คุณฝ่ามือและฝ่าเท้าร้อนระดับใด ?");
 
         //yhang
         question.add("คุณหน้าแดงระดับใด ?");
         question.add("คุณหายใจแรงระดับใด ?");
         question.add("คุณรู้สึกตัวร้อนระดับใด?");
-        question.add("คุณคอแห้งระดับใด ?");
         question.add("คุณชอบด่ืมนํ้าเย็นระดับใด?");
         question.add("คุณท้องผูกระดับใด ?");
         question.add("คุณปัสสาวะเหลืองเข้มระดับใด ?");
         question.add("ฝ่ามือและฝ่าเท้าของคุณเย็นง่ายระดับใด ?");
         question.add("คุณปัสสาวะบ่อยตอนกลางคืนระดับใด ?");
-        question.add("คุณหน้าตาซีดระดับใด ?");
+
 
         Text_question.setText(index + ". " + question.get(1));
-
+        button_previous.setVisibility(View.GONE);
     }
 
-    public String Checkpoint() {
+    public int checkPoint() {
+//        if(radioButton1.isChecked()) {
+//            return 1;
+//        } else if(radioButton2.isChecked()){
+//            return 2;
+//        } else if (radioButton3.isChecked()){
+//            return 3;
+//        } else if (radioButton4.isChecked()) {
+//            return 4;
+//        } else {
+//            return 5;
+//        }
 
-        if (radioButton1.isChecked()) {
-            return "1";
-        } else if(radioButton2.isChecked()){
-            return "2";
-        }else if (radioButton3.isChecked()){
-            return "3";
-        }else if (radioButton4.isChecked()) {
-            return "4";
-        }else {;
-            return "5";
+        for(int i=1; i<radioAnswer.length; i++) {
+            if (radioAnswer[i].isChecked()) {
+                Log.e("eiei"+Integer.toString(i), Integer.toString(i));
+                return i;
+            }
         }
+        return 0;
     }
 
 }
